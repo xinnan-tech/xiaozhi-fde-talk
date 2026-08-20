f9ad157d65d5218b5a643d66e24c4cadcd15f2c7
lww9029 <756794909@qq.com>
2026-08-20 09:47:51 +0800

    fix(api): 修复前端代理重复拼接 API 前缀

--- frontend/src/api/utils.ts
+++ frontend/src/api/utils.ts
@@ -1,4 +1,4 @@
 export const baseUrlApi = (url: string) =>
   process.env.NODE_ENV === "development"
-    ? `/api${url}`
+    ? `${url}`
     : `http://192.168.4.119:8000${url}`;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -29,8 +29,12 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
         "/api": {
           // 这里填写后端地址
           target: "http://192.168.4.119:8000",
+          changeOrigin: true
+        },
+        "/ws": {
+          target: "ws://192.168.4.119:8000",
           changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, "")
+          ws: true
         }
       },
       // 预热文件以提前转换和缓存结果，降低启动期间的初始页面加载时长并防止转换瀑布
